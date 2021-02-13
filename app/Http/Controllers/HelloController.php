18aa96f14998150aa7f1095b52739df266c66226
tetsu <cda07c5117@gmail.com>
2021-02-13 12:44:03 +0900

    リスト1-5

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -14,4 +14,9 @@ class HelloController extends Controller
         ];
         return view('hello.index',$data);
     }
+
+    public function other()
+    {
+        return redirect()->route('hello');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello/', [HelloController::class, 'index']);
+Route::get('/hello', [HelloController::class, 'index'])->name('hello');
+Route::get('/hello/other', [HelloController::class, 'other']);
