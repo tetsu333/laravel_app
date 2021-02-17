0d919e46884d2fd04a5c7c971c039c408c4b4afe
tetsu <cda07c5117@gmail.com>
2021-02-17 23:39:47 +0900

    リスト1-44

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ use App\Http\Middleware\HelloMiddleware;
 
 Route::get('/sample', [SampleController::class, 'index'])->name('sample');
 Route::get('/hello', [HelloController::class, 'index'])->name('hello');
-Route::get('/hello/{msg}', [HelloController::class, 'other']);
+Route::post('/hello/other', [HelloController::class, 'other']);
