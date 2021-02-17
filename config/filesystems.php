d7020ed64554866cf0c6a9f39d5d43916763042e
tetsu <cda07c5117@gmail.com>
2021-02-18 00:01:33 +0900

    リスト1-48

--- config/filesystems.php
+++ config/filesystems.php
@@ -52,6 +52,12 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
         ],
 
+        'logs' => [
+            'driver' => 'local',
+            'root' => storage_path('logs'),
+            'url' => env('APP_URL') . '/storage/logs',
+        ],
+
     ],
 
     /*
